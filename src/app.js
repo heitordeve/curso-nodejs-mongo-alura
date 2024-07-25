5c50f5b38054e2eb7cf9526a2785df5d8eafc6ad
heitor.souza@usekim.com.br <heitor.souza@usekim.com.br>
2024-07-25 11:00:27 -0300

    Acessando a coleção livros

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import conectBaseDados from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
 
 const conexao = await conectBaseDados();
 
@@ -15,36 +16,19 @@ const app = express();
 
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        nomeLivro: "Livro 1"
-    },
-    {
-        id: 2,
-        nomeLivro: "Livro 2"
-    },
-]
-
-function buscarLivrosporID(id){
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
-
 app.get("/", (req, res) => {
     res.status(200).send("Curso de NodeJS");
+});
+
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
     const index = buscarLivrosporID(req.params.id);
     res.status(200).json(livros[index]);
 });
-
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-});
-
 
 app.post("/livros", (req,res) => {
     livros.push(req.body);
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const livroSchema = new Mongoose.Schema({
+const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     titulo: { type: String, required: true },
     editora: { type: String },
